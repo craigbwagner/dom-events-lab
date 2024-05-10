2fcfb415478bf9c3050f7c7e52d0633a03867200
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-10 11:25:37 -0400

    added initial display and cached element references

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
+const display = {
+	value: 0,
+};
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
+const displayElement = document.querySelector('.display');
+const buttonElement = document.querySelector('button');
+const numberElement = document.querySelector('number');
+const addElement = document.querySelector('add');
+const subtractElement = document.querySelector('subtract');
+const multiplyElement = document.querySelector('multiply');
+const divideElement = document.querySelector('divide');
+const clearElement = document.querySelector('clear');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
+const setDisplay = () => {
+	displayElement.textContent = display.value;
+};
+
+setDisplay();
