2925c83ca3ca1b68ab7f413fecf5023514a85eaa
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-11 18:49:32 -0400

    added setDisplay, setOperator, compute functions, renamed display object to calculator

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
 /*-------------------------------- Constants --------------------------------*/
-const display = {
-	value: 0,
+const calculator = {
+	displayValue: '_',
+	operation: null,
+	inputs: [], //pressing an operator will push the first value into this array
 };
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 const displayElement = document.querySelector('.display');
-const buttonElement = document.querySelector('button');
-const numberElement = document.querySelector('number');
-const addElement = document.querySelector('add');
-const subtractElement = document.querySelector('subtract');
-const multiplyElement = document.querySelector('multiply');
-const divideElement = document.querySelector('divide');
-const clearElement = document.querySelector('clear');
+
+// const inputEl = document.querySelector('.input');
+// const buttonElements = inputEl.children;
+
+const numberElement = document.querySelector('.number');
+const addElement = document.querySelector('.add');
+const subtractElement = document.querySelector('.subtract');
+const multiplyElement = document.querySelector('.multiply');
+const divideElement = document.querySelector('.divide');
+const clearElement = document.querySelector('.clear');
 
 /*----------------------------- Event Listeners -----------------------------*/
+'click',
+	() => {
+		console.log('click');
+	};
 
 /*-------------------------------- Functions --------------------------------*/
-const setDisplay = () => {
-	displayElement.textContent = display.value;
+const buttonResponse = (event) => {
+	// if (event.target.class === 'number') {
+	//    switch (event.target.textContent) {
+	//    }
+	// }
+	// if (event.target.class === 'add') {
+	// }
+	// if (event.target.class === 'subtract') {
+	// }
+	// if (event.target.class === 'multiply') {
+	// }
+	// if (event.target.class === 'divide') {
+	// }
+	// if (event.target.class === 'clear') {
+	// }
+	// if (event.target.class === 'equals') {
+	// }
 };
+
+const setDisplay = () => {
+	displayElement.textContent = calculator.displayValue;
+};
+
+const setOperator = () => {};
+
+const compute = () => {};
 
 setDisplay();
